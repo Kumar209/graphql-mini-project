9ab435c4916b0cf5ba1789086b5b7e5f737ed8be
Kumar209 <76429886+Kumar209@users.noreply.github.com>
2023-07-08 20:16:14 +0530

    created schema in which created project and client object type and their queries

---
+++ server/schema/schema.js
@@ -0,0 +1,78 @@
+const { projects , clients } = require("../sample");
+
+const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList } = require('graphql');
+
+
+//Project Type
+const ProjectType = new GraphQLObjectType({
+    name: 'Project',
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        description: {type: GraphQLString},
+        status: {type: GraphQLString},
+
+        //In below you have setup relationship between client and project
+        client: {
+            type: ClientType,
+            resolve(parent, args){
+                return clients.find(client => client.id === parent.clientId);  //clientId is present in porject array of smaple data
+            }
+        }
+    })
+});
+
+
+//Client Type
+const ClientType = new GraphQLObjectType({
+    name: 'Client',
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        email: {type: GraphQLString},
+        phone: {type: GraphQLString},
+    })
+});
+
+//To query any client details
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parent, args){
+                //sample data projects
+                return projects;
+            }
+        },
+
+        project: {
+            type: ProjectType,
+            args: {id: {type: GraphQLID}},
+            resolve(parent, args) {
+                return projects.find(project =>project.id === args.id);
+            }
+        },
+
+
+        clients: {
+            type: new GraphQLList(ClientType),
+            resolve(parent, args){
+                return clients;
+            }
+        },
+
+        client: {
+            type: ClientType,
+            args: {id: {type: GraphQLID}},
+            resolve(parent, args) {
+                return clients.find(client => client.id === args.id);
+            }
+        }
+    }
+});
+
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
