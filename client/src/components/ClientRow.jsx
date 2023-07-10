da62c6caf952ae0138a53035d6fb6fda5fae0aec
Kumar209 <76429886+Kumar209@users.noreply.github.com>
2023-07-10 20:49:54 +0530

    completed project

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -2,7 +2,7 @@ import { FaTrash } from 'react-icons/fa';
 import { useMutation } from '@apollo/client';
 import { DELETE_CLIENT } from '../mutations/clientMutations.jsx';
 import { GET_CLIENTS } from '../queries/clientQueries';
-// import { GET_PROJECTS } from '../queries/projectQueries';
+import { GET_PROJECTS } from '../queries/projectQueries';
 
 export default function ClientRow({ client }) {
   const [deleteClient] = useMutation(DELETE_CLIENT, {
@@ -11,18 +11,18 @@ export default function ClientRow({ client }) {
 
     //Problem - whenever we delete client , we have to refresh our UI to see the changes. Now to solve that we have to 2 methods
     //1: Call another query called GET_CLIENTS
-    // refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }],
+    refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }],
 
     //2: Update the cache
-    update(cache, { data: { deleteClient } }) {
-      const { clients } = cache.readQuery({ query: GET_CLIENTS });
-      cache.writeQuery({
-        query: GET_CLIENTS,
-        data: {
-          clients: clients.filter((client) => client.id !== deleteClient.id),
-        },
-      });
-    },
+    // update(cache, { data: { deleteClient } }) {
+    //   const { clients } = cache.readQuery({ query: GET_CLIENTS });
+    //   cache.writeQuery({
+    //     query: GET_CLIENTS,
+    //     data: {
+    //       clients: clients.filter((client) => client.id !== deleteClient.id),
+    //     },
+    //   });
+    // },
   });
 
   return (
